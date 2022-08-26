49d30cd5b215ebd02b651a1457a49fefbb380b6e
kaenovsky <martinkaen@gmail.com>
2022-08-25 23:56:04 -0300

    Refactor old code

--- 111-tecnicas-programacion/python_files/clase-03/ejercicio02.py
+++ 111-tecnicas-programacion/python_files/clase-03/ejercicio02.py
@@ -6,7 +6,12 @@ ingresando su base y altura
 
 base = int(input("ingrese la base del rectángulo: "))
 altura = int(input("ingrese la altura del rectángulo: "))
-perimetro = (base + altura) * 2
+perimetro = (2 * base + 2 * altura)
+area = (base * altura)
 
 print("el perimetro de su rectángulo es: {} ".format(perimetro))
+print("el área de su rectángulo es: {} ".format(area))
+
+print()
+print('Fin del programa')
 
