d262e88fe9e3c292a15e92d0476926b1e0d46ad5
kaenovsky <martinkaen@gmail.com>
2022-09-03 13:33:59 -0300

    OOP homework 04

--- 121-oop/python_files_oop/tarea-clase-persona/main_persona.py
+++ 121-oop/python_files_oop/tarea-clase-persona/main_persona.py
@@ -1,22 +1,33 @@
 # tarea: crear una clase Persona con atributos y metodos
 
 class Persona:
-    def __init__(self,nombre,edad,dni):
+    def __init__(self, nombre:str, edad:int, dni:str):
         self.nombre = nombre
         self.edad = edad
         self.dni = dni
 
-    def mostrar_edad(self):
+    def mostrar_edad(self) -> int:
         return self.edad
     
-    def es_mayor_edad(self):
+    def es_mayor_edad(self) -> bool:
         if self.edad >= 18:
             return True
         else:
             return False
 
-magno = Persona('Alejandro', 37, '29245680')
-cesar = Persona('Julio', 21, '20245680')
+# creamos objetos del tipo Persona
 
-print('La edad de {} es {}. Su DNI es {}'.format(magno.nombre,magno.mostrar_edad(),magno.dni))
-print('La edad de {} es {}. Su DNI es {}'.format(cesar.nombre,cesar.mostrar_edad(),cesar.dni))
+magno = Persona('Alejandro', 26, '29245680')
+cesar = Persona('Julio', 15, '20245680')
+polo = Persona('Marco', 41, '34556780')
+
+# guardamos las personas en una List y la recorremos
+
+users = [magno, cesar, polo]
+
+for u in users:
+    if u.es_mayor_edad():
+        res = 'sí'
+    else:
+        res = 'no'
+    print('{} de {} años, DNI n° {}: {} es mayor de edad'.format(u.nombre, u.mostrar_edad(), u.dni, res))
