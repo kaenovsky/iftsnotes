bddefbac63da5892075bc05448b64a4bf4378a91
kaenovsky <martinkaen@gmail.com>
2022-12-11 12:42:37 -0300

    codeacademy practice +

---
+++ 121-oop/practice/codeacademy_python_course/11-access-modifiers.py
@@ -0,0 +1,15 @@
+# Access Modifications
+# https://www.codecademy.com/courses/python-for-programmers/articles/access-modifications-python
+
+class ClassSchedule:
+    def __init__(self, course, instructor):
+        self.course = course
+        self.instructor = instructor
+
+    def display_course(self):
+        print(f'Course: {self.course}, Instructor: {self.instructor}')
+
+# All members here are accessible outside of the class.
+
+sched = ClassSchedule('Math', 'Mr. Xin')
+sched.display_course()
