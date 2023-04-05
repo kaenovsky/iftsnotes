6572b8aac074717c56efc6bc0d74ca21d6a24929
kaenovsky <martinkaen@gmail.com>
2023-04-05 20:32:17 -0300

    Include class mobile 05 + java file

---
+++ 211-moviles/java-sandbox/TP1/Impares.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Impares {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese un numero para saber si es par o impar:");
+		Integer num = scanner.nextInt();
+
+		if(num % 2 == 0) {
+		
+			System.out.println("El número es par");
+
+		} else {
+		
+			System.out.println("El número es impar");
+
+		}
+
+	}
+
+}
